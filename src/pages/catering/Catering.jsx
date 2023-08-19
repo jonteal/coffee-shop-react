6308076781d081d3fd7412defa1bf30c9a98d851
Jon Jackson <jonjacksonvibes@gmail.com>
2023-08-19 10:13:20 -0500

    added a contact form and started catering menu page

--- src/pages/catering/Catering.jsx
+++ src/pages/catering/Catering.jsx
@@ -1,7 +1,24 @@
+import MainImage from "../../assets/images/catering.jpg";
 import "./catering.css";
 
 const Catering = () => {
-  return <div>Catering</div>;
+  return (
+    <div className="container">
+      <img
+        src={MainImage}
+        alt="Coffee shop"
+        className="catering-main-image"
+      ></img>
+      <div className="catering-text-container">
+        <h1 className="text-slate-50 text-opacity-80 font-bold catering-title-header font-serif">
+          Hillside
+        </h1>
+        <h2 className="text-slate-50 text-opacity-80 font-bold catering-subtitle-header font-serif">
+          Catering Menu
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Catering;
--- src/pages/locations/Locations.jsx
+++ src/pages/locations/Locations.jsx
@@ -1,7 +1,13 @@
 import "./locations.css";
 
 const Locations = () => {
-  return <div>Locations</div>;
+  return (
+    <div>
+      <h1 className="text-zinc-400 text-opacity-20 font-bold locations-header font-serif">
+        Locations
+      </h1>
+    </div>
+  );
 };
 
 export default Locations;
