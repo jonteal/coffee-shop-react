f3e85a3f45a8cedae35764f0938784427c894489
Jon Jackson <jonjacksonvibes@gmail.com>
2023-08-20 14:09:20 -0500

    image grid on home page is made

---
+++ src/components/homeImageGrid/HomeImageGrid.jsx
@@ -0,0 +1,35 @@
+import Image1 from "../../assets/images/homeGrid/image1.jpg";
+import Image2 from "../../assets/images/homeGrid/image2.jpg";
+import Image3 from "../../assets/images/homeGrid/image3.jpg";
+import Image4 from "../../assets/images/homeGrid/image4.jpg";
+import Image5 from "../../assets/images/homeGrid/image5.jpg";
+import Image6 from "../../assets/images/homeGrid/image6.jpg";
+import Image7 from "../../assets/images/homeGrid/image7.jpg";
+import Image8 from "../../assets/images/homeGrid/image8.jpg";
+
+import "./homeImageGrid.css";
+
+const HomeImageGrid = () => {
+  return (
+    <div className="image-grid-container">
+      <div className="row">
+        <div className="column">
+          <img src={Image1} />
+          <img src={Image2} />
+          <img src={Image3} />
+        </div>
+        <div className="column">
+          <img src={Image4} />
+          <img src={Image5} />
+        </div>
+        <div className="column">
+          <img src={Image6} />
+          <img src={Image7} />
+          <img src={Image8} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeImageGrid;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,59 @@
+import { Link } from "react-router-dom";
 import Slider from "../../components/slider/Slider";
+import CoffeeIcon from "../../assets/coffee-logo.jpg";
+import { FaArrowRight } from "react-icons/fa";
 
 import "./home.css";
+import HomeImageGrid from "../../components/homeImageGrid/HomeImageGrid";
 
 const Home = () => {
   return (
     <div>
       <div className="slider-container">
         <Slider />
+      </div>
+      <div className="flex justify-center mt-5">
+        <img src={CoffeeIcon} alt="" className="w-1/12" />
+      </div>
+      <div className="flex flex-col justify-center">
+        <h1 className="text-slate-500 text-opacity-80 home-about-subtitle-header font-serif my-2">
+          About Hillside
+        </h1>
+        <hr className="home-about-line-break" />
+        <div className="flex flex-col items-center">
+          <p className="w-1/2 my-2 text-slate-500 text-opacity-80">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi aut
+            deleniti, fugiat dolor soluta dolore debitis eius doloremque at vero
+            aperiam.
+          </p>
+          <p className="w-1/2 my-2 text-slate-500 text-opacity-80">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi aut
+            deleniti, fugiat dolor soluta dolore debitis eius doloremque at vero
+            aperiam.
+          </p>
+          <p className="w-1/2 my-2 text-slate-500 text-opacity-80">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi aut
+            deleniti, fugiat dolor soluta dolore debitis eius doloremque at vero
+            aperiam.
+          </p>
+          <p className="w-1/2 my-2 text-slate-500 text-opacity-80">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi aut
+            deleniti, fugiat dolor soluta dolore debitis eius doloremque at vero
+            aperiam.
+          </p>
+
+          <Link to="/about">
+            <div className="flex flex-row items-center">
+              <span className="text-yellow-800 text-opacity-80 my-4 text-3xl font-serif italic">
+                learn more
+              </span>
+              <FaArrowRight className="my-4 ml-3 text-2xl text-yellow-800 text-opacity-80" />
+            </div>
+          </Link>
+          <div className="home-image-grid-container flex flex-row justify-center">
+            <HomeImageGrid />
+          </div>
+        </div>
       </div>
     </div>
   );
